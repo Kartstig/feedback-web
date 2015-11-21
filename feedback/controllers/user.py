1e05b8b0109ca3800db9a572ffe10e60dc15569e
Herman Singh <kartstig@gmail.com>
2015-11-21 15:57:26 -0500

    Add more stuff

--- feedback/controllers/user.py
+++ feedback/controllers/user.py
@@ -3,7 +3,7 @@
 
 from flask import request, redirect, url_for, current_app, \
     render_template, flash, Blueprint, Response, jsonify
-
+from flask.ext.login import login_user, current_user, logout_user
 from feedback.models.User import User
 from feedback.services.UserService import UserService
 
@@ -12,3 +12,23 @@ user = Blueprint('user', __name__)
 @user.route('/api/user/<int:id>', methods=['GET'])
 def user_get(id):
     return jsonify(UserService().filter_by(id=id).serialize())
+
+@user.route('/api/user/all', methods=['GET'])
+def user_all():
+    return jsonify(dict(users=UserService().serialize()))
+
+@user.route('/login', methods=['POST'])
+def user_login():
+    errors = {}
+    us = UserService()
+    if request.method == 'POST':
+        try:
+            user = us.by_username(request.form['username'])
+            if user.valid_password(request.form['password']):
+                return jsonify(user)
+            else:
+                return abort(401)
+        except:
+            return abort(500)
+    else:
+        return abort(400)
