30cdfaea69ebac6b8da8064b6a66d31be9abdea5
Scott Ziegler <aslan235@gmail.com>
2015-11-21 19:52:31 -0500

    added some test sms code

--- feedback/controllers/index.py
+++ feedback/controllers/index.py
@@ -6,9 +6,17 @@ from flask import request, redirect, url_for, current_app, \
 from flask.ext.login import current_user
 
 from feedback.config import app_config
+from feedback.twilio.sms import SMS
 
 index = Blueprint('index', __name__)
 
 @index.route('/', methods=['GET'])
 def main():
     return render_template("index.html", current_user=current_user)
+
+@index.route('/respond', methods=['GET', 'POST'])
+def respond():
+	sms = SMS()
+	(from_number, msg) = sms.receive_msg(request)
+	sms.send_msg(from_number, "I hear you, {}, you said {}".format(from_number, msg))
+
